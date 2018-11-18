fc857e2f12eaa5cc4c02b6ea16625853d091b676
Jason White <jwhite@dwtunnel.com>
2018-11-18 10:51:16 -0500

    added api router to routes module exports

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const ApiV1Router = require('./api-v1.router');
 const AuthenticationRouter = require('./authentication.router');
 const UsersRouter = require('./users.router');
 
 module.exports = {
+  ApiV1Router,
   AuthenticationRouter,
   UsersRouter
 }
