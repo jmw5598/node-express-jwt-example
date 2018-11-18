eb49211d4ec51cdb16f443dbbaae6011fa86cb7e
Jason White <jwhite@dwtunnel.com>
2018-11-18 15:39:27 -0500

    added findAll and findOne methods to UsersRepository

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,10 +1,38 @@
 'use strict';
 
-const Users = require('../models');
+const { User, Role } = require('../models');
 
 class UsersRepository {
 
   constructor() {}
+
+  findAll() {
+    return User.findAll({
+      include: [{
+        model: Role,
+        as: 'roles',
+        attributes: ['role'],
+        through: { attributes: [] }
+      }],
+      attributes: {
+        exclude: ['id', 'password']
+      }
+    });
+  }
+
+  findOne(username) {
+    return User.findOne({
+      where: { username: username },
+      include: [{
+        model: Role, as: 'roles', attributes: ['role'],
+        through: { attributes: [] }
+      }]
+    })
+  }
+
+  save(user) {
+
+  }
 
 }
 
