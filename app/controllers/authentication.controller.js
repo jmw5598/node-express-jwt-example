30ed66209ef8b77b0f55a1baf4f1e6349d07cb10
Jason White <jwhite@dwtunnel.com>
2018-11-22 19:04:50 -0500

    created authenticate method

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,38 +1,39 @@
 'use strict';
 
-const { UsersRepository } = require('../controllers');
+const bcrypt = require('bcrypt');
+const { UsersRepository } = require('../repositories');
+const { JwtService } = require('../services');
 
 class AuthenticationController {
 
   constructor() {}
 
   authenticate(req, res) {
-    return res.status(200).send({ message: 'Successfully Authenticated' });
-    // const username = req.body.username;
-    // const password = req.body.password;
-    //
-    // let user = UsersRepository.findOne(username);
-    //
-    // // maybe have this as a service?
-    // let valid = user.then(user) => {
-    //   if(user)
-    //     return bcrypt.compare(password, user.dataValues.password);
-    //   else
-    //     throw err;
-    // })
-    //
-    // return Promise.all([user, valid])
-    //   .then([user, valid]) => {
-    //     if(valid) {
-    //       let payload = { user: user.dataValues.username, roles: user.dataValues.roles };
-    //       return res.status(200).send({ token: JwtService.sign(payload) });
-    //     } else {
-    //       throw err;
-    //     }
-    //   })
-    //   .catch((error) => res.status(401).send({ message: 'Invalid username/password' }));
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let user = UsersRepository.findOne(username);
+
+    let valid = user.then((user) => {
+      if (user)
+        return bcrypt.compare(password, user.dataValues.password);
+      else
+        throw err;
+    }).catch((error) => console.log("error: " + error));
+
+    return Promise.all([user, valid])
+      .then(([user, valid]) => {
+        if(valid) {
+          let payload = { user: user.dataValues.username, roles: user.dataValues.roles };
+          return res.status(200).send({ token: JwtService.sign(payload) });
+        } else {
+          throw err;
+        }
+      })
+      .catch((error) => res.status(401).send({ message: 'Invalid username/password' }));
   }
 
 }
 
-module.exports = new AuthenticationController();
+module.exports = new AuthenticationController(UsersRepository);
